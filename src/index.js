efae72a44a01595a2080a460fe6c85d875fb98ae
Aryan <aryanguptaleo@gmail.com>
2020-05-13 06:15:35 +0530

    Lint fixes

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-
-fetch(`./Data/file.json`)
-.then((r) => r.json())
-.then((data) =>{
-  console.log(data);
-    ReactDOM.render(<App appData={data}/>, document.getElementById('root'));
-})
+fetch('./Data/file.json')
+    .then((r) => r.json())
+    .then((data) => {
+        ReactDOM.render(
+            // eslint-disable-next-line react/jsx-filename-extension
+            <App appData={data} />,
+            document.getElementById('root')
+        );
+    });
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
